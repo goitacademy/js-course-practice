e66f748a7610ef721123761c6428b5abb2fb3119
Alexander Repeta <alexander.repeta@gmail.com>
2024-06-03 16:09:33 +0300

    Add m01t01

---
+++ module-1/1.js
@@ -0,0 +1,12 @@
+/**
+ * Створіть функцію greet, яка приймає ім'я
+ * та повертає привітання у форматі "Hello, name!".
+ * Де замість name потрібно підставити ім'я користувача.
+ */
+
+function greet(useraname) {
+  return `Hello, ${useraname}!`;
+}
+
+console.log(greet("Jacob")); // Hello, Jacob!
+console.log(greet("Andre")); // Hello, Andre!
