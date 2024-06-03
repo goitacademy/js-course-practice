9164f072bd670e3bc7d3f878d61f8ef4856bdce0
Alexander Repeta <alexander.repeta@gmail.com>
2024-06-03 16:23:41 +0300

    Add m01t03

---
+++ module-1/3.js
@@ -0,0 +1,12 @@
+/**
+ * Створіть функцію getAverage, яка приймає три числа
+ * і повертає їх середнє арифметичне.
+ */
+
+function getAverage(a, b, c) {
+  return (a + b + c) / 3;
+}
+
+console.log(getAverage(15, 7, 11)); // 11
+console.log(getAverage(5, 4, 9)); // 6
+console.log(getAverage(35, 70, 45)); // 50
