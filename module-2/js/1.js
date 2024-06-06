d72aa9a79fb3edd709e1a0802e768a6c77c29203
Alexander Repeta <alexander.repeta@gmail.com>
2024-06-06 13:06:50 +0300

    Add m02t01

---
+++ module-2/js/1.js
@@ -0,0 +1,17 @@
+/**
+ * Створіть функцію checkEvenOdd, яка приймає число
+ * і повертає рядок "Even" для парних чисел та "Odd" для непарних.
+ */
+
+function checkEvenOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(checkEvenOdd(4)); // Even
+console.log(checkEvenOdd(6)); // Even
+console.log(checkEvenOdd(7)); // Odd
+console.log(checkEvenOdd(11)); // Odd
